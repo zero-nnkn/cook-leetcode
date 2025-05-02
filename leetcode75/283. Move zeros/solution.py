04dac3ba802a649e18187ad7cd1a62e9712ebfe5
zero-nnkn <nnkhoinguyen9@gmail.com>
2025-05-02 23:58:13 +0700

    ✨ leetcode75 - streak 2

---
+++ leetcode75/283. Move zeros/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # Approach 1: Like bubble sort
+        non_zero_pos = len(nums) - 1
+        for i in range(len(nums) - 1, -1, -1):
+            if nums[i] == 0:
+                nums[i : non_zero_pos] = nums[i + 1: non_zero_pos + 1] 
+                nums[non_zero_pos] = 0
+                non_zero_pos -= 1

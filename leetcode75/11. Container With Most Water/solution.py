9a4afa5aaff49d0a8e774f5d51f658a16a4e9084
zero-nnkn <nnkhoinguyen9@gmail.com>
2025-05-04 18:46:04 +0700

    ✨ leetcode75 - streak 4

---
+++ leetcode75/11. Container With Most Water/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        max_area = 0
+        l, r = 0, len(height) - 1
+        while l < r:
+            area = (r - l) * min(height[l], height[r]) 
+            if area > max_area:
+                max_area = area
+            
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+                
+        return max_area

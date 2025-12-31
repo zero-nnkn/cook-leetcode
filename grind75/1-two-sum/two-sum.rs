ab4b179018c86af704ef5ce5e33983b5581c71d8
zero-nnkn <nnkhoinguyen9@gmail.com>
2025-12-31 21:53:58 +0700

    init grind75

---
+++ grind75/1-two-sum/two-sum.rs
@@ -0,0 +1,18 @@
+use std::collections::HashMap;
+
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map = HashMap::new();
+
+        for (i, &num) in nums.iter().enumerate() {
+            let complement = target - num;
+            if let Some(&index) = map.get(&complement) {
+                return vec![index as i32, i as i32];
+            }
+            map.insert(num, i);
+        }
+        
+        return vec![]
+    }
+}

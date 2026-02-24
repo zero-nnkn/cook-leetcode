9c0e5098b65ddfac12a1f6a87ee02683a8674897
Khoi-Nguyen Nguyen-Ngoc <nnkhoinguyen9@gmail.com>
2026-02-24 23:40:57 +0700

    Time: 42 ms (62.36%) | Memory: 19.3 MB (22.11%) - LeetSync

--- grind75/278-first-bad-version/first-bad-version.py
+++ grind75/278-first-bad-version/first-bad-version.py
@@ -6,14 +6,13 @@ class Solution:
         if n==1:
             return 1
         
-        left = 0
+        left = 1
         right = n
         while left < right:
-            mid = (left + right)//2 + 1
+            mid = left + (right - left) // 2
             if isBadVersion(mid):
-                if not isBadVersion(mid -1):
-                    return mid
-                right = mid - 1
+                right = mid
             else:
                 left = mid + 1
+                
         return left

09cfcdd3bd52471a44b9914e6810fe5f87927f65
Khoi-Nguyen Nguyen-Ngoc <nnkhoinguyen9@gmail.com>
2026-02-26 23:32:35 +0700

    Time: 54 ms (48.81%) | Memory: 22.4 MB (83.16%) - LeetSync

--- grind75/141-linked-list-cycle/linked-list-cycle.py
+++ grind75/141-linked-list-cycle/linked-list-cycle.py
@@ -6,12 +6,24 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        visited_node = []
-        cur = head
-        while cur is not None:
-            if cur in visited_node:
+        # visited_node = []
+        # cur = head
+        # while cur is not None:
+        #     if cur in visited_node:
+        #         return True
+        #     visited_node.append(cur)
+        #     cur = cur.next
+        # return False 
+
+        # Slow and Fast Pointers
+        if head is None or head.next is None:
+            return False
+
+        slow, fast = head, head.next
+        while slow is not None and fast is not None and fast.next is not None:
+            if slow == fast:
                 return True
-            visited_node.append(cur)
-            cur = cur.next
-        return False 
+            slow = slow.next
+            fast = fast.next.next
+        return False
         

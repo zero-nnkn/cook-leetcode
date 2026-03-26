e848224c549d88d32402c216b31f3ca6f9743313
Khoi-Nguyen Nguyen-Ngoc <nnkhoinguyen9@gmail.com>
2026-03-26 23:47:11 +0700

    Time: 0 ms (100.00%) | Memory: 21.5 MB (20.20%) - LeetSync

--- grind75/57-insert-interval/insert-interval.py
+++ grind75/57-insert-interval/insert-interval.py
@@ -1,25 +1,26 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-
+        if len(intervals) == 0:
+            return [newInterval]
 
         final_intervals = []
+        n = len(intervals)
         idx = 0
-        while idx < len(intervals) and newInterval[0] > intervals[idx][1]:
+
+        # 1. Add all intervals before newInterval
+        while idx < n and intervals[idx][1] < newInterval[0]:
             final_intervals.append(intervals[idx])
             idx += 1
-
-        if idx >= len(intervals):
-            return intervals + [newInterval]
         
-        while idx < len(intervals) and newInterval[1] >= intervals[idx][0]:
+        while idx < n and newInterval[1] >= intervals[idx][0]:
             newInterval = [
                 min(newInterval[0], intervals[idx][0]),
                 max(newInterval[1], intervals[idx][1])
             ]
             idx += 1
-
         final_intervals.append(newInterval)
-        if idx < len(intervals):
+
+        if idx < n:
             final_intervals.extend(intervals[idx:])
 
         return final_intervals
